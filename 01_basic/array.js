a0c60b48b04a006408eba16af45da98d0b8b2339
sachintharu <107626983+sachintharu@users.noreply.github.com>
2024-01-02 16:21:35 +0000

    container cofig done

---
+++ 01_basic/array.js
@@ -0,0 +1,23 @@
+const age =[10,30,20,50,60];
+console.log(age[0]);
+//another method to create array
+// const cars =[]
+// cars[0]="volvo";
+// cars[1]="saab";
+// cars[2]="BMW";
+// console.log(cars);
+//another method to create array using new keyword
+//const cars1 = new Array("saab", "volvo", "BMW");
+// const cars ={first:"saab", second:"volvo",third:"BMW"};
+// cars[0]="opel";
+// console.log(cars);
+// console.log(cars.first);
+//method
+//length property of an array return the nbr of elements
+const fruits=["banana","orange", "apple", "mango"];
+// console.log(fruits.length-1);
+for (let i=0;i<fruits.length;i++)
+{
+    console.log(fruits[i]);
+}
+
